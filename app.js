48256fade42eea075c41710c254b014d38d074a8
Shahin Mahmud <rsuitsit@gmail.com>
2019-08-29 15:41:06 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(3000);
